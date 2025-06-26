7df4eecd300f7ea29fd46587b11e3ea211244ad0
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-06-26 11:39:58 -0700

    with views

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :sessions, only: %i[new create destroy]
+
+  root "users#index"
 end
