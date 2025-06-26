bb2301a1bd5882220cfd45a81192816e1b534879
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-06-26 12:06:18 -0700

    working

--- app/channels/online_channel.rb
+++ app/channels/online_channel.rb
@@ -1,5 +1,8 @@
 class OnlineChannel < ApplicationCable::Channel
   def subscribed
+    # Stream from the same channel name that the model broadcasts to
+    stream_from "online_users"
+
     current_user&.update!(status: "online", last_online_at: Time.current)
     super
   end
