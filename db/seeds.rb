93e2859314251a3d99d9ef5773eea38c455fb860
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-06-26 11:25:14 -0700

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%w[
+  paul
+  amy
+  sarah
+  shan
+  emily
+  bert
+  alice
+  nate
+  gloria
+  gustav
+].each do |username|
+  User.where(username: username).first_or_create!
+end
