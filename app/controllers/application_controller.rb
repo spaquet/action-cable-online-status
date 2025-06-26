2248bd83a031ffd12dee144b3b1f81497cfadef3
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-06-26 11:23:03 -0700

    Adding session related methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
+  def current_user
+    if cookies.encrypted[:user_id]
+      @user ||= User.find(cookies.encrypted[:user_id])
+    end
+  rescue => e
+    cookies.delete(:user_id)
+  end
+  helper_method :current_user
 end
